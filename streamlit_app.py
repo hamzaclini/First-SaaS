b6fd816af2153ee579e959779517d05cc49c8662
hamzaclini <155651110+hamzaclini@users.noreply.github.com>
2024-06-04 10:42:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,9 @@ st.title("The most Fantabulous SaaS Ever! 🚀")
 
 add_auth(required=True)
 
-st.write(f"Subscription Status: {st.session_state.user.subscribed}")
+#st.write(f"Subscription Status: {st.session_state.user.subscribed}")
 st.write("🎉 Yay! You're all set and subscribed! 🎉")
-st.write(f"By the way, your email is: {st.session_state.email}")
+#st.write(f"By the way, your email is: {st.session_state.email}")
 
 
 
